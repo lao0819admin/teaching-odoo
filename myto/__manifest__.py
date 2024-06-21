e30f211ef8e0f4873d2395817d90bb81bb04b445
lao0819admin <lyah.andrey@gmail.com>
2024-06-21 17:40:48 +0300

    Initial commit

--- myto/__manifest__.py
+++ myto/__manifest__.py
@@ -1,1 +1,31 @@
-{}
+{
+    'name': 'My test module',
+    'summary': 'UA TEST GUN MY AR',
+
+    'author': 'xtos tam ua',
+    'website': 'https://des-tam.ua',
+
+    'category': 'Customizations',
+    'license': 'OPL-1',
+    'version': '17.0.0.0.1',
+
+    'depends': [
+        'base',
+        ],
+
+    'external_dependencies': {'python': [], },
+    'data': [
+    ],
+    'demo': [
+    ],
+
+    'installable': True,
+    'auto_install': False,
+    'aplication': False,
+    'images': [
+       '',
+      '',
+    ],
+    'price': 0,
+    'curency': 'EUR',
+}
